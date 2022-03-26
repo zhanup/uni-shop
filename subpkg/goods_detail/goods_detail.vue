d6d307fae5f8ba6b5003f50ae99409c3bb349f7b
zhanup <2925309929@qq.com>
2022-03-26 15:36:02 +0800

    完成了 home 首页的开发

---
+++ subpkg/goods_detail/goods_detail.vue
@@ -0,0 +1,19 @@
+<template>
+	<view>
+		GoodsDetail
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			};
+		}
+	}
+</script>
+
+<style lang="scss">
+
+</style>
