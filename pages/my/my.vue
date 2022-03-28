6cb7fb8911676387473713dfbdb36e66672546d1
zhanup <2925309929@qq.com>
2022-03-28 14:24:53 +0800

    完成了登录和支付功能的开发

--- pages/my/my.vue
+++ pages/my/my.vue
@@ -1,22 +1,29 @@
 <template>
-	<view>
-		My
+	<view class=".my-container">
+		<!-- 用户未登录时，显示登录组件 -->
+    <my-login v-if="!token"></my-login>
+
+    <!-- 用户登录后，显示用户信息组件 -->
+    <my-userinfo v-else></my-userinfo>
 	</view>
 </template>
 
 <script>
-  import badgeMix from '@/mixins/tabbar-badge.js'
-  
-	export default {
-    mixins: [badgeMix],
-		data() {
-			return {
-				
-			};
-		}
-	}
+import { mapState } from 'vuex'
+import badgeMix from '@/mixins/tabbar-badge.js'
+
+export default {
+  mixins: [badgeMix],
+  computed: {
+    // 从 m_user 模块中导入需要的 token 字符串
+    ...mapState('m_user', ['token']),
+  }
+}
 </script>
 
 <style lang="scss">
-
+page,
+.my-container {
+  height: 100%;
+}
 </style>
